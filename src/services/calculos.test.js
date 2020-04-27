199666ea0da82f460bd0cf93dc4af443da55ea78
ismaelpadilla <padillaismael92@gmail.com>
2020-04-27 17:55:22 -0300

    Se reestructuran los tests de los impuestos

--- src/services/calculos.test.js
+++ src/services/calculos.test.js
@@ -22,25 +22,28 @@ describe("Probar cálculo de aportes BPS", () => {
     }
   );
 
-  describe("Aplican topes jubilatorios", () => {
-    test.each([
-      [188411, 28261.65],
-      [200000, 28261.65],
-    ])("Calcula aportes BPS correctamente", (salarioNominal, esperadoJubilatorio) => {
-      /**
-       * El aporte jubilatorio se aplica hasta el tope 188411 (valor 2020).
-       */
-      const resultado = calcularAportesBPS(salarioNominal, false, false);
+  test.each([
+    [188411, 28261.65],
+    [200000, 28261.65],
+  ])("Aplican topes jubilatorios", (salarioNominal, esperadoJubilatorio) => {
+    /**
+     * El aporte jubilatorio se aplica hasta el tope 188411 (valor 2020).
+     */
+    const resultado = calcularAportesBPS(salarioNominal, false, false);
 
-      // Lo esperado deberia estar a un valor razonable de lo obtenido, para evitar errores de redondeo
-      expect(resultado.aportesJubilatorios).toBeCloseTo(esperadoJubilatorio);
-    });
+    // Lo esperado deberia estar a un valor razonable de lo obtenido, para evitar errores de redondeo
+    expect(resultado.aportesJubilatorios).toBeCloseTo(esperadoJubilatorio);
   });
+});
 
+describe("Probar cálculo de IRPF", () => {
   test.each([
     [20000, 0, 0, 3000, 900, 20, 0, { impuestoFranja: [0, 0, 0, 0, 0, 0, 0, 0], deducciones: 3920 }, 0],
     [40000, 0, 0, 6000, 1800, 40, 0, { impuestoFranja: [0, 837, 0, 0, 0, 0, 0, 0], deducciones: 7840 }, 53],
     [80000, 0, 0, 12000, 3600, 80, 0, { impuestoFranja: [0, 1356, 3389, 4084, 0, 0, 0, 0], deducciones: 15680 }, 7574],
+    [80000, 1, 0, 12000, 3600, 80, 0, { impuestoFranja: [0, 1356, 3389, 4084, 0, 0, 0, 0], deducciones: 20576 }, 7183],
+    [80000, 0, 1, 12000, 3600, 80, 0, { impuestoFranja: [0, 1356, 3389, 4084, 0, 0, 0, 0], deducciones: 25471 }, 6791],
+    [80000, 1, 1, 12000, 3600, 80, 0, { impuestoFranja: [0, 1356, 3389, 4084, 0, 0, 0, 0], deducciones: 30367 }, 6399],
   ])(
     "Calcula IRPF correctamente",
     (
