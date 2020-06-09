f52b0df5e3eb9ebe819fdb986e33af0b972615c8
Ismael Padilla <padillaismael92@gmail.com>
2020-06-09 19:59:11 -0300

    Se agrega prettier al proyecto

--- src/App.test.js
+++ src/App.test.js
@@ -1,7 +1,7 @@
-import React from 'react';
-import { render } from '@testing-library/react';
-import App from './App';
+import React from "react";
+import { render } from "@testing-library/react";
+import App from "./App";
 
-test('renders learn react link', () => {
+test("renders learn react link", () => {
   render(<App />);
 });
